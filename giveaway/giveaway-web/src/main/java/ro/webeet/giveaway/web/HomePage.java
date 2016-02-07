a6d06b2d2ed52e97835abe433d3dda4d5519be60
Andrei Chirinciuc <andrei.chirinicuc@gmail.com>
2016-02-07 16:16:31 +0200

    remove unused methods

--- giveaway/giveaway-web/src/main/java/ro/webeet/giveaway/web/HomePage.java
+++ giveaway/giveaway-web/src/main/java/ro/webeet/giveaway/web/HomePage.java
@@ -2,28 +2,14 @@ package ro.webeet.giveaway.web;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
 
 	private static final long serialVersionUID = -5031543042965269269L;
 
-	public HomePage() {
-	}
-
-	public HomePage(IModel<?> model) {
-		super(model);
-	}
-
-	public HomePage(PageParameters parameters) {
-		super(parameters);
-	}
-
 	@Override
 	protected void onInitialize() {
 		super.onInitialize();
-
 		add(new Label("label", "Hello World!"));
 	}
 }
